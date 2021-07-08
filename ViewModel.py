56882763572109fcd5e356af081f9793e7e41f0c
Doom_raven <danila.matveev.2017@gmail.com>
2021-07-08 22:48:45 +0300

    Added comments to ViewModel.py

--- ViewModel.py
+++ ViewModel.py
@@ -8,12 +8,12 @@ class ViewModel:
         self.xray = ""  # изоражение рентгена
         self.percentage_of_defeat = 0  # процент поражения лёгких
 
-    def input_of_image(self, path):
+    def input_of_image(self, path): # заглушка для кнопки загрузки
         self.xray = Image.open(path)
 
-    def analysis(self):
+    def analysis(self): # заглушка для кнопки начала анализа
         self.xray = Image.open("Output image.jpg")
         return self.percentage_of_defeat
 
-    def get_image(self):
-        return ImageTk.PhotoImage(self.xray.resize((400, 500), Image.ANTIALIAS))
+    def get_image(self,x,y): # возвращение изображения нужного размера
+        return ImageTk.PhotoImage(self.xray.resize((x, y), Image.ANTIALIAS))

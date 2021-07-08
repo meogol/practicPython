bed0c1668e753f4c96ea07fb590a944c01cf45e3
Doom_raven <danila.matveev.2017@gmail.com>
2021-07-08 22:59:31 +0300

    Added right comments to ViewModel.py

--- ViewModel.py
+++ ViewModel.py
@@ -8,12 +8,15 @@ class ViewModel:
         self.xray = ""  # изоражение рентгена
         self.percentage_of_defeat = 0  # процент поражения лёгких
 
-    def input_of_image(self, path): # заглушка для кнопки загрузки
+    """заглушка для кнопки загрузки"""
+    def input_of_image(self, path):
         self.xray = Image.open(path)
 
-    def analysis(self): # заглушка для кнопки начала анализа
+    """заглушка для кнопки начала анализа"""
+    def analysis(self):
         self.xray = Image.open("Output image.jpg")
         return self.percentage_of_defeat
 
-    def get_image(self,x,y): # возвращение изображения нужного размера
+    """возвращение изображения нужного размера"""
+    def get_image(self,x,y):
         return ImageTk.PhotoImage(self.xray.resize((x, y), Image.ANTIALIAS))

5737cfb9aec2e592301950d3245ec469edc73172
meow <avs99.98@gmail.com>
2021-07-13 20:03:58 +0300

    Auto stash before rebase of "origin/uiQt"

--- ViewModel.py
+++ ViewModel.py
@@ -1,6 +1,5 @@
-import random
 from PIL import Image, ImageTk
-
+import pydicom as dicom
 import back.model_manager as model_manager
 
 
@@ -12,17 +11,16 @@ class ViewModel:
         self.model = model_manager.AiModel('back\\')
 
     """заглушка для кнопки загрузки"""
-
     def input_of_image(self, path):
-        self.xray = Image.open(path)
+        ds = dicom.dcmread(path)
+
+        self.xray = ds.pixel_array
 
     """Возвращает результат работы нейросети(0-кот, 1-собака)"""
-
     def analysis(self):
         return self.model.get_result(self.xray)
 
     """возвращение изображения нужного размера"""
-
     def get_image(self, x, y):
         return ImageTk.PhotoImage(self.xray.resize((x, y), Image.ANTIALIAS))
 
--- back/model_manager.py
+++ back/model_manager.py
@@ -3,16 +3,17 @@ import tensorflow as tf
 from PIL import Image
 from tensorflow import keras
 
-
 class AiModel:
     def __init__(self, path):
-        self.model = keras.models.load_model(path+'gosha_model')
-        self.model.load_weights(path+'model_weights')
+        self.model = keras.models.load_model(path + 'gosha_model')
+        self.model.load_weights(path + 'model_weights')
 
     def get_result(self, image):
         image = image.resize((256, 256), Image.ANTIALIAS)
         res = self.model.predict(tf.expand_dims(image, axis=0))
 
         return np.argmax(res)
+
+
 
 

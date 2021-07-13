77317a6bafaba793893aa85f4327b80a6bed7ef2
Evgenii <eugenefolkast@gmail.com>
2021-07-13 20:00:20 +0300

    Comments translated in russian

--- View.py
+++ View.py
@@ -15,15 +15,17 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.vm = ViewModel()
 
-        """ Connection of download image button to slot input_of_image in ViewModel"""
+        # Связь кнопки загруки изображения (downloadBtn) с методом открытия файлового диалога (open_file_dialog)
         self.ui.downloadBtn.clicked.connect(self.open_file_dialog)
 
-        """ Connection of start analyze button to slot analysis in ViewModel"""
+
+        # Связь кнопки analyzeBtn и метода analysis_process
         self.ui.analyzeBtn.clicked.connect(self.analysis_process)
 
-        """ Changing background color of image holder """
+        # Изменение цвета заднего фона поля изображения
         self.ui.imgHolder.setStyleSheet("background-color: #D9D9D9")
 
+    """ Вызов метода анализа из файла ViewModel, отображение возвращённого изображения из нейросети и текста"""
     def analysis_process(self):
         # self.vm.analysis()
         # """ Placing image in image holder from slot get_image in ViewModel"""
@@ -32,11 +34,12 @@ class mywindow(QtWidgets.QMainWindow):
         # self.ui.imgHolder.resize(417, 586)
         # self.show()
 
-        """ Displaying information about lungs on picture in text window """
+
         percent = self.vm.analysis()
         self.ui.textBrowser.setPlainText(str(percent))
         self.show()
 
+    """ Открытие диалогового окна выбора файла, проверка на наличие пути, вывод выбранного изображения на экран """
     def open_file_dialog(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image files (*.jpg)")[0]
 
@@ -45,7 +48,7 @@ class mywindow(QtWidgets.QMainWindow):
 
         self.vm.input_of_image(fname)
 
-        """ Placing original image in image holder """
+        #Вывод изображения на экран с помощью pixmap
         pixmap = QPixmap(fname)
         self.ui.imgHolder.setPixmap(pixmap)
         self.ui.imgHolder.resize(417, 586)

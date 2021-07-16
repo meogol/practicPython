7987b687f6de36dbb1cc215410a7fa94fb5915e1
meow <avs99.98@gmail.com>
2021-07-16 12:08:35 +0300

    gosha v8

--- back/ai_manager.py
+++ back/ai_manager.py
@@ -4,7 +4,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     'data\\train',
     validation_split=0.2,
     subset="training",
-    batch_size=15,
+    batch_size=10,
     shuffle=True,
     seed=324893,
     image_size=(256, 256),
@@ -15,7 +15,7 @@ validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
     'data\\validation',
     validation_split=0.2,
     subset="validation",
-    batch_size=15,
+    batch_size=10,
     shuffle=True,
     seed=324893,
     image_size=(256, 256),
@@ -24,7 +24,7 @@ validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 test_ds = tf.keras.preprocessing.image_dataset_from_directory(
     'data\\test',
-    batch_size=15,
+    batch_size=10,
     image_size=(256, 256),
     color_mode='rgb',
 
--- back/model.py
+++ back/model.py
@@ -9,23 +9,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 inputs = keras.Input(shape=(256, 256, 3), name="img")
 
-x = layers.Conv2D(32, 3, activation="relu")(inputs)
-x = layers.Conv2D(64, 3, activation="relu")(x)
+x = layers.Conv2D(128, 3, activation="relu", padding="same")(inputs)
+x = layers.Conv2D(128, 3, activation="relu", padding="same")(x)
 block_1_output = layers.MaxPooling2D(3)(x)
 
-x = layers.Conv2D(64, 3, activation="relu", padding="same")(block_1_output)
 x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
-block_2_output = layers.add([x, block_1_output])
-
-x = layers.Conv2D(64, 3, activation="relu", padding="same")(block_2_output)
 x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
-block_3_output = layers.add([x, block_2_output])
+x = layers.MaxPooling2D(3)(x)
 
-x = layers.Conv2D(64, 3, activation="relu")(block_3_output)
+
+x = layers.Conv2D(32, 3, activation="relu", padding="same")(x)
+x = layers.Conv2D(32, 3, activation="relu", padding="same")(x)
+x = layers.MaxPooling2D(3)(x)
+
+x = layers.Conv2D(16, 3, activation="relu")(x)
 x = layers.GlobalAveragePooling2D()(x)
 
-x = layers.Dense(256, activation="relu")(x)
-x = layers.Dense(256, activation="relu")(x)
+x = layers.Dense(16, activation="relu")(x)
+x = layers.Dense(16, activation="relu")(x)
 
 outputs = layers.Dense(2, activation='softmax')(x)
 
